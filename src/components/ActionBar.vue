95a01ac501674cd0aa6957c20a979f970dad9661
Jordan <jordan.elizeu77@gmail.com>
2021-10-05 10:32:17 -0300

    Componentes inutilizados deletados e adicionado componentes padrões

---
+++ src/components/ActionBar.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="action-bar">
+      <h1>Olá mundo !!!</h1>
+  </div>
+</template>
+
+<script>
+export default {
+    name:'ActionBar',
+    data(){
+        return{
+
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/ViewLinksBio.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="view-links-bio">
+      <h1>Olá mundo !!!</h1>
+  </div>
+</template>
+
+<script>
+export default {
+    name:'VieLinksBio',
+    data(){
+        return{
+
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
